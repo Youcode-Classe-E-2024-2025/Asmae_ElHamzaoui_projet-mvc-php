896e45f14c97a33a39a878009215c49ad53cad12
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-02-04 15:36:54 +0100

    l'instatlation de twig

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-// Définission des routes nécessaires
+// Définission des routes nécessaires%
 $router->addRoute('GET', '/', 'HomeController@index');
 $router->addRoute('GET', '/article/{id}', 'ArticleController@show');
 
