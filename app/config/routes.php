95098963bf35e71c3c35f6cf4529b5627b3e7ed1
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-02-07 13:14:42 +0100

    modification des fichier du dossier config

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,5 +1,16 @@
 <?php
-$routes = [
-    '/' => 'HomeController@index',
-    '/article/(\d+)' => 'ArticleController@show',
-];
+
+// Définition des routes pour le Front Office
+$router->addRoute('GET', '/', 'front\HomeController', 'index'); // Page d'accueil
+$router->addRoute('GET', '/login', 'front\AuthController', 'login'); // Page de connexion
+$router->addRoute('POST', '/login', 'front\AuthController', 'processLogin'); // Traitement de la connexion
+$router->addRoute('GET', '/sign-up', 'front\AuthController', 'signUp'); // Page d'inscription
+$router->addRoute('POST', '/sign-up', 'front\AuthController', 'processSignUp'); // Traitement de l'inscription
+
+// Définition des routes pour le Back Office (Admin)
+$router->addRoute('GET', '/admin/dashboard', 'back\DashboardController', 'index'); // Dashboard Admin
+$router->addRoute('GET', '/admin/users', 'back\UserController', 'index'); // Gestion des utilisateurs
+$router->addRoute('GET', '/admin/articles', 'back\ArticleController', 'index'); // Gestion des articles
+
+// Route pour la déconnexion
+$router->addRoute('GET', '/logout', 'front\AuthController', 'logout'); // Déconnexion
