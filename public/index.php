0ebfac5650210cfe7d6fef85d7ca2e9e80507fc2
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-02-07 13:17:39 +0100

    configuration du fichier index.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,25 @@
 <?php
-require_once '../app/core/Router.php';
-require_once '../app/core/Controller.php';
-require_once '../app/core/Model.php';
-require_once '../app/core/View.php';
 
-// Démarre la gestion des routes
-$router = new Router();
-$router->handleRequest();
+// Autoloading des classes
+require_once '../vendor/autoload.php';
+
+// Chargement des fichiers de configuration
+require_once '../app/config/config.php';
+
+// Démarrer la session
+session_start();
+
+use Dotenv\Dotenv;
+
+// Charger le fichier .env
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+// Instanciation du routeur
+$router = new App\Core\Router();
+
+// Chargement des routes (nous allons ajouter ici les routes définies dans routes.php)
+require_once '../app/config/routes.php';
+
+// Dispatch de la requête
+$router->dispatch($_SERVER['REQUEST_URI']);
